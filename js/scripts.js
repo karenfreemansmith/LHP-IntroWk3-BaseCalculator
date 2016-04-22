3b1727e7a3662697c6cf9f8275a1302bd2d6a886
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-04-21 20:02:20 -0700

    create form and connect to java script - alert values

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,6 @@
 //alert("jQuery is working on " + $("h1").text()); //test for jQuery linked and loaded correctly
+
+$("#convert").click(function() {
+  alert($("#myNumber").val());
+  alert($("input[name=base]:radio").val());
+});
